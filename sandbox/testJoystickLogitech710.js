55dfa0040ed6b9a64a35c51ea8e528c924a3f2f9
root <born2net@gmail.com>
2015-09-21 19:36:17 -0700

    cleanup Joystick Algor

--- sandbox/testJoystickLogitech710.js
+++ sandbox/testJoystickLogitech710.js
@@ -1,12 +1,13 @@
 #!/usr/local/bin/node
 
-console.log('remember to run /root/pytongpio/socketPython.py');
+log('remember to run /root/pytongpio/socketPython.py');
 
 var net = require('net');
 
 var HOST = 'localhost';
 var PORT = 5432;
 var SERVER_CONNECT = 1;
+var DEBUG = 0;
 
 var skip = 0;
 
@@ -16,22 +17,17 @@ var skip = 0;
 if (SERVER_CONNECT) {
     var socket = new net.Socket();
     socket.connect(PORT, HOST, function () {
-        console.log('We are connected to the Receiver!');
-        setTimeout(function(){
-            var joyX = 0;
-            var joyY = 0;
-            runMotor(0, 0, 'fwd');
-        },2000);
+        log('We are connected to the Receiver!');
     });
 
 // this event handler is called when data is received on the socket
     socket.on('data', function (data) {
-        //console.log('DATA: ' + data);
+        //log('DATA: ' + data);
     });
 
 // if the socket is closed, this handler will be called
     socket.on('close', function () {
-        console.log('Connection closed');
+        log('Connection closed');
     });
 
 // this handler will be called when the process receives the sigint signal,
@@ -42,6 +38,11 @@ if (SERVER_CONNECT) {
 //});
 }
 
+function log(msg, level) {
+    if (DEBUG >= level) {
+        console.log(msg);
+    }
+}
 
 var Joystick = require("joystick-logitech-f710");
 
@@ -69,127 +70,121 @@ Joystick.create("/dev/input/js0", function (err, joystick) {
 
     joystick.setMaximumAxesPosition(1017);
     joystick.on("button:a:press", function () {
-        console.log("a1");
+        log("a1",3);
     });
     joystick.on("button:a:release", function () {
-        console.log("a2");
+        log("a2",3);
     });
     joystick.on("button:a:release", function () {
-        console.log("a2");
+        log("a2",3);
     });
     joystick.on("button:a:release", function () {
-        console.log("a2");
+        log("a2",3);
     });
 
     joystick.on("stick:1:vertical:up", function (position) {
-        console.log("1: " + position);
+        log("1: " + position,3);
     });
     joystick.on("stick:1:vertical:down", function (position) {
-        console.log("2: " + position);
+        log("2: " + position,3);
     });
     joystick.on("stick:1:vertical:zero", function (position) {
-        console.log("3: " + position);
+        log("3: " + position,3);
     });
 
     joystick.on("stick:2:vertical:up", function (position) {
-        console.log("7: " + position);
+        log("7: " + position,3);
     });
     joystick.on("stick:2:vertical:down", function (position) {
-        console.log("8: " + position);
+        log("8: " + position,3);
     });
     joystick.on("stick:2:vertical:zero", function (position) {
-        console.log("9: " + position);
+        log("9: " + position,3);
     });
     joystick.on("stick:2:horizontal:right", function (position) {
-        console.log("10: " + position);
+        log("10: " + position,3);
     });
     joystick.on("stick:2:horizontal:left", function (position) {
-        console.log("11: " + position);
+        log("11: " + position,3);
     });
     joystick.on("stick:2:horizontal:zero", function (position) {
-        console.log("12: " + position);
+        log("12: " + position,3);
     });
 
     joystick.on("stick:3:horizontal:right", function (position) {
-        console.log("16: " + position);
+        log("16: " + position,3);
     });
     joystick.on("stick:3:horizontal:left", function (position) {
-        console.log("17: " + position);
+        log("17: " + position,3);
     });
     joystick.on("stick:3:horizontal:zero", function (position) {
-        console.log("18: " + position);
+        log("18: " + position,3);
     });
-
 
 
     // MOTORS DRIVE //
     joystick.on("stick:1:horizontal:right", function (position) {
         joyY = Math.abs((position / 2) + Y_CENTER);
-        console.log("STICK DOWN: " + position + ' joyY: ' + joyY);
+        log("STICK DOWN: " + position + ' joyY: ' + joyY,3);
 
     });
     joystick.on("stick:1:horizontal:left", function (position) {
         joyY = Math.abs((position / 2) - Y_CENTER);
-        console.log("STICK UP: " + position + ' joyY: ' + joyY);
+        log("STICK UP: " + position + ' joyY: ' + joyY,3);
 
     });
     joystick.on("stick:3:vertical:up", function (position) {
         joyX = Math.abs((position / 2) - X_CENTER);
-        console.log("STICK LEFT: " + position + ' joyX: ' + joyX);
+        log("STICK LEFT: " + position + ' joyX: ' + joyX,3);
 
     });
     joystick.on("stick:3:vertical:down", function (position) {
         joyX = Math.abs((position / 2) + X_CENTER);
-        console.log("STICK RIGHT: " + position + ' joyX: ' + joyX);
+        log("STICK RIGHT: " + position + ' joyX: ' + joyX,3);
     });
     joystick.on("stick:3:vertical:zero", function (position) {
-        console.log("A: " + position);
+        log("A: " + position,3);
         //joyX = X_CENTER;
         //joyY = Y_CENTER;
     });
     joystick.on("stick:1:horizontal:zero", function (position) {
-        console.log("B: " + position);
+        log("B: " + position,3);
         //joyX = X_CENTER;
         //joyY = Y_CENTER;
     });
     joystick.on("button:lb:press", function () {
-        console.log("lb1");
         runMotor(1, 1, 'sharpLeft');
         skip = 1;
     });
     joystick.on("button:lb:release", function () {
-        console.log("lb2");
         skip = 0;
         runMotor(0, 0, 'fwd');
     });
     joystick.on("button:rb:press", function () {
-        console.log("rb1");
         runMotor(1, 1, 'sharpRight');
         skip = 1;
     });
-       joystick.on("button:rb:release", function () {
-        console.log("rb2");
+    joystick.on("button:rb:release", function () {
         skip = 0;
         runMotor(0, 0, 'fwd');
     });
     joystick.on("button:ls:press", function () {
-        console.log("stop");
+        log("stop",2);
         skip = 0;
         runMotor(0, 0, 'fwd');
     });
     joystick.on("button:ls:release", function () {
-        console.log("stop");
+        log("stop",2);
         skip = 0;
         runMotor(0, 0, 'fwd');
     });
-
 
 
 });
 
 
 setInterval(function () {
-    console.log('x ' + joyX + ' y ' + joyY);
+    log('x ' + joyX + ' y ' + joyY,1);
     if (skip)
         return;
     CommandDifferentialDrive2(joyX, joyY);
@@ -207,9 +202,8 @@ function fixDec(val) {
 function runMotor(leftMotor, rightMotor, direction) {
     leftMotor = Math.abs(fixDec(leftMotor));
     rightMotor = Math.abs(fixDec(rightMotor));
-    console.log('direction ' + direction + ' leftMotor ' + leftMotor + ' rightMotor ' +rightMotor);
+    log('direction ' + direction + ' leftMotor ' + leftMotor + ' rightMotor ' + rightMotor,1);
     var value = "MOTOR-" + leftMotor + '-' + rightMotor + '-' + direction;
-    console.log(value);
     if (SERVER_CONNECT)
         socket.write(value);
 }
@@ -221,23 +215,24 @@ var CommandDifferentialDrive2 = function (x, y) {
 
     moveY = leftMotor = rightMotor = (Y_CENTER - y) / Y_CENTER;
 
-    console.log('ZZZ ' + rightMotor + ' ' + leftMotor);;
-    if (rightMotor==1&&leftMotor==1)
+    log('ZZZ ' + rightMotor + ' ' + leftMotor,3);
+
+    if (rightMotor == 1 && leftMotor == 1)
         return;
 
     // sharp turn: enable following lines if you wish to mix to stick sharp turns on extrem left and right stick movements
     /*
-    if (y > THRESHOLD_LOW && y < THRESHOLD_HIGH) {
-        if (x < THRESHOLD_LOW) {
-            runMotor(1, 1, 'sharpLeft');
-            return;
-        }
-        if (x > THRESHOLD_HIGH) {
-            runMotor(1, 1, 'sharpRight');
-            return
-        }
-    }
-    */
+     if (y > THRESHOLD_LOW && y < THRESHOLD_HIGH) {
+     if (x < THRESHOLD_LOW) {
+     runMotor(1, 1, 'sharpLeft');
+     return;
+     }
+     if (x > THRESHOLD_HIGH) {
+     runMotor(1, 1, 'sharpRight');
+     return
+     }
+     }
+     */
 
     // fwd
     if (moveY > 0) {
