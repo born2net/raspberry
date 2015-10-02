6dc4995c3c1f4446a99e4e798c063f26888e375e
root <born2net@gmail.com>
2015-10-02 15:25:51 -0700

    onoff added as well as ServoBlaster

--- sandbox/button.js
+++ sandbox/button.js
@@ -1,9 +1,18 @@
 #!/usr/local/bin/node
 
-var Gpio = require('onoff').Gpio;
-var button = new Gpio(21, 'in', 'rising');
 
-var a = 0;
-button.watch(function(err, value) {
-    console.log('button ' + value)
+var gpio = require("pi-gpio");
+
+gpio.open(40, "input", function(err) {     // Open pin 16 for output
+    gpio.read(40,function(e) {          // Set pin 16 high (1)
+        //gpio.close(16);                     // Close pin 16
+        console.log('PIN 40 ' + e);
+
+    });
+});
+
+
+gpio.read(16, function(err, value) {
+    if(err) throw err;
+    console.log(value); // The current state of the pin
 });
