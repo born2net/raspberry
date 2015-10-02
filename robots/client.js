64faad733da6b755e549335b22e98715d73b28f5
root <born2net@gmail.com>
2015-10-01 21:21:12 -0700

    inherit from event emitter

--- robots/client.js
+++ robots/client.js
@@ -1,6 +1,7 @@
 #!/usr/local/bin/node
 var exec = require('child_process').exec;
-var noderob = require('noderob').createNodeRob({
+
+var nodeRob = require('noderob').create({
     debug: 0,
     serverConnect: 1,
     host: 'localhost',
@@ -8,12 +9,16 @@ var noderob = require('noderob').createNodeRob({
     pollInterval: 25
 });
 
-var commBroker = require('noderob').createComBroker();
+nodeRob.on('ABC', function (e) {
+    console.log('GOTAA ' + e);
+});
 
-noderob.initSocket();
-noderob.initServos();
-noderob.initJoystick(function (joystick) {
-    var self = noderob;
+nodeRob.logSomething('firing');
+
+nodeRob.initSocket();
+nodeRob.initServos();
+nodeRob.initJoystick(function (joystick) {
+    var self = nodeRob;
 
     // override specific behavior
     joystick.removeAllListeners("button:crossup:press");
@@ -28,30 +33,29 @@ noderob.initJoystick(function (joystick) {
     });
 });
 
-commBroker.listen('LCD:right',function(e){
-    noderob.log('r',0);
+nodeRob.on('LCD:right', function (e) {
+    nodeRob.log('r', 0);
 });
-commBroker.listen('LCD:left',function(e){
-    noderob.log('l',0);
+nodeRob.on('LCD:left', function (e) {
+    nodeRob.log('l', 0);
 });
-commBroker.listen('LCD:up',function(e){
-    noderob.log('u',0);
+nodeRob.on('LCD:up', function (e) {
+    nodeRob.log('u', 0);
 });
-commBroker.listen('LCD:down',function(e){
-    noderob.log('d',0);
+nodeRob.on('LCD:down', function (e) {
+    nodeRob.log('d', 0);
 });
-commBroker.listen('LCD:select',function(e){
-    noderob.log('s',0);
+nodeRob.on('LCD:select', function (e) {
+    nodeRob.log('s', 0);
     var child = exec('shutdown now');
 });
 
-
 var a = 1;
 setInterval(function () {
-    noderob.setLCD(Math.random() + '\n' + a++, 'magenta');
+    nodeRob.setLCD(Math.random() + '\n' + a++, 'magenta');
 }, 1000);
-noderob.start();
-noderob.log('Starting NodeRob...', 1, 'yellow');
+nodeRob.start();
+nodeRob.log('Starting NodeRob...', 1, 'yellow');
 
 
 
