5bd2691a0e00043c8564defe3534cd82fbcd2135
root <born2net@gmail.com>
2015-10-23 18:00:52 -0700

    GPS coords

--- gps/getgps.js
+++ gps/getgps.js
@@ -1,7 +1,9 @@
 #!/usr/local/bin/node
 
 /**
+ *
  *  SignagePlayer event trigger upon real time GPS coordinates
+ *
  **/
 
 var exec = require('child_process').exec;
@@ -21,7 +23,7 @@ var intervalSeconds = 2;
  @param {String} i_param
  **/
 function sendRemoteCommand(i_domain, i_user, i_password, i_stationId, i_eventName, i_param) {
-    //var url = 'https://sun.signage.me/WebService/sendCommand.ashx?i_user=d39@ms.com&i_password=123123&i_stationId=46&i_command=event&i_param1=gps&i_param2=34.154595901466685,-118.78676801919937&callback=?';
+    //var url = 'https://sun.signage.me/WebService/sendCommand.ashx?i_user=d39@ms.com&i_password=xxxxxx&i_stationId=46&i_command=event&i_param1=gps&i_param2=34.154595901466685,-118.78676801919937&callback=?';
     var url = i_domain + '/WebService/sendCommand.ashx?i_user=' + i_user + '&i_password=' + i_password + '&i_stationId=' + i_stationId + '&i_command=event' + '&i_param1=' + i_eventName + '&i_param2=' + i_param + '&callback=?';
     //console.log(url);
     jsonpClient(url, function (err, data) {
@@ -49,19 +51,25 @@ function sendLocalCommand(i_ip, i_port, i_eventName, i_param) {
         });
 }
 
+var c = 0;
+
 /** Get live GPS data via interval **/
 setInterval(function () {
-    exec('python getgps.py', function callback(error, stdout, stderr) {
+    c++;
+    exec('python /root/gps/getgps.py', function callback(error, stdout, stderr) {
         var data = JSON.parse(stdout);
         var p = data.la + ',' + data.lon;
-        console.log('Sending ' + p + ' on ' + data.time);
+
+        // console.log('Sending ' + p + ' on ' + data.time);
 
         // send remote command through the web to an online SignagePlayer
-        sendRemoteCommand('https://sun.signage.me', 'd39@ms.com', '123123', '46', 'gps', p);
+        ///// sendRemoteCommand('https://sun.signage.me', 'd39@ms.com', 'xxxxxx', '49', 'gps', p);
 
         // send remote command locally to a player that's possibly offline
-        sendLocalCommand('192.168.42.27', '1024', 'gps', p);
+        ///// sendLocalCommand('192.168.42.27', '1026', 'gps', p);
+
+        console.log('configure /root/gps/getgps.js with your settings...');
     });
-}, intervalSeconds * 1000);
+}, intervalSeconds * 5000);
 
 
